dcc4c12c66949706c1c3144fcab60b5423e70fa4
Laércio Junior <imlaerciojunior@gmail.com>
2023-10-18 22:29:34 -0300

    Projeto Finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,4 +1,5 @@
--- Active: 1695690091017@@127.0.0.1@1433
+-- Active: 1697666064661@@127.0.0.1@3306
+
 
 -- TABELA DE USUARIOS
 CREATE TABLE users (
@@ -17,10 +18,10 @@ DROP TABLE users;
 
 -- INSERIR OS NOVOS USUARIOS
 INSERT INTO users (id, name, email, password) VALUES
-('u003', 'Shoyo', 'shoyo@email.com', 'whiscassache'),
-('u004', 'Biju', 'biju@email.com', 'boladepelo'),
-('u005', 'Bruno', 'bruno@email.com', 'meumaridolindo'),
-('u006', 'Carol', 'carol@email.com', 'novaeuropa');
+('u001', 'Bruno', 'bruno@email.com', 'bruno123'),
+('u002', 'Biju', 'biju@email.com', 'biju123'),
+('u003', 'Shoyo', 'shoyo@email.com', 'shoyo123'),
+('u004', 'Marina', 'marina@email.com', 'marina123');
 
 -- TABELA DE PRODUTOS
 CREATE TABLE products (
@@ -39,12 +40,12 @@ DROP TABLE products;
 
 -- INSERE NOVOS PRODUTOS
 INSERT INTO products (id, name, price, description, image_url) VALUES
-('prod003', 'robo aspirador', 1234.00, 'Robô Aspirador de Pó KaBuM! Smart 700, Mapeamento IR 360º, Controle via Aplicativo, Google Assistant e Alexa', 'https://images.kabum.com.br/produtos/fotos/155444/aspirador-de-po-robo-ir-360-kabum-smart-700-preto_1628768893_m.jpg'),
-('prod004', 'placa de video gamer', 1800.00, 'Placa de Vídeo MSI NVIDIA GeForce RTX 3060 Ventus', 'https://images7.kabum.com.br/produtos/fotos/384627/placa-de-video-msi-nvidia-geforce-rtx-3060-ventus-2x-12gb-gddr6-dlss-ray-tracing-912-v397-272_1663850312_m.jpg'),
-('prod005', 'playstation 5', 3500.00, 'Console Sony Playstation 5, Edição Digital', 'https://images.kabum.com.br/produtos/fotos/238670/console-sony-playstation-5-edicao-digital_1634132113_m.jpg'),
-('prod006', 'Macbook Pro Apple 16', 12034.00, 'macbook pro de 16 polegadas', 'https://images2.kabum.com.br/produtos/fotos/sync_mirakl/479192/Macbook-Pro-Apple-16-Chip-M1-Max-32GB-SSD-1TB-Cinza-Espacial-Mk1a3bz-A_1694541516_m.jpg'),
-('prod007', 'Samsung Galaxy Z Flip5', 6234.00, 'Smartphone Samsung Galaxy Z Flip5 512gb 5g Tela Dobrável 6.7 Câmera Dupla 12mp Câmera Selfie 10mp Dual Chip Android 13, Grafite', 'https://images.kabum.com.br/produtos/fotos/magalu/480251/Smartphone-Samsung-Z-Flip-5-512GB-Grafite-5G-Snapdragon-8GB-RAM-6-7-C-m-Dupla-Selfie-10MP-Dual-Chip_1692195175_m.jpg'),
-('prod008', 'XBOX 360 one', 2600.00, 'Espaço de armazenamento: 512 Gb Conexões: HDMI-In/Out, Portas usb, Wi-Fi embutido Tensão: Bivolt', 'https://images-americanas.b2w.io/produtos/3052435356/imagens/console-xbox-serie-s-ssd512gb-1controle-rrs-00006/3052435364_1_xlarge.jpg');
+('prod001', 'robo aspirador', 1234.00, 'Robô Aspirador de Pó KaBuM! ', 'https://images.kabum.com.br/produtos.jpg'),
+('prod002', 'placa de video gamer', 1800.00, 'Placa de Vídeo MSI NVIDIA GeForce ', 'https://images.kabum.com.br/produtos2.jpg'),
+('prod003', 'playstation 5', 3500.00, 'Console Sony Playstation 5, Edição Digital', 'https://images.kabum.com.br/produtos3.jpg'),
+('prod004', 'Macbook Pro Apple 16', 12034.00, 'macbook pro de 16 polegadas', 'https://images.kabum.com.br/produtos4.jpg'),
+('prod005', 'Samsung Galaxy Z Flip5', 6234.00, 'Smartphone Samsung Galaxy Z Flip5', 'https://images.kabum.com.br/produtos5.jpg'),
+('prod006', 'XBOX 360 one', 2600.00, 'Espaço de armazenamento: 512 Gb', 'https://images.kabum.com.br/produtos6.jpg');
 
 -- BUSCA POR NOME GAME DOS PRODUTOS
 SELECT * FROM products
@@ -57,7 +58,7 @@ WHERE id = 'u003';
 
 -- DELETAR PRODUCT BY ID
 DELETE FROM products
-WHERE id = 'prod007';
+WHERE id = 'prod006';
 
 -- EDITAR PRODUCT BY ID
 UPDATE products
@@ -89,17 +90,16 @@ DROP TABLE purchases;
 -- INTRODUZIR PEDIDO
 INSERT INTO purchases (id, buyer_id, total_price, product_id, product_description)
 VALUES
-('p001', 'u003', 100,'prod003', 'Robô Aspirador de Pó KaBuM! Smart 700, Mapeamento IR 360º, Controle via Aplicativo, Google Assistant e Alexa'),
-('p002', 'u004', 300, 'prod003','Robô Aspirador de Pó KaBuM! Smart 700, Mapeamento IR 360º, Controle via Aplicativo, Google Assistant e Alexa'),
-('p003', 'u005', 110, 'prod003', 'Robô Aspirador de Pó KaBuM! Smart 700, Mapeamento IR 360º, Controle via Aplicativo, Google Assistant e Alexa'),
-('p004', 'u006', 200,'prod003', 'Robô Aspirador de Pó KaBuM! Smart 700, Mapeamento IR 360º, Controle via Aplicativo, Google Assistant e Alexa');
+('p001', 'u001', 100,'prod003', 'Robô Aspirador de Pó KaBuM!'),
+('p002', 'u003', 300, 'prod003','Robô Aspirador de Pó KaBuM!'),
+('p003', 'u004', 110, 'prod003', 'Robô Aspirador de Pó KaBuM!'),
+('p004', 'u002', 200,'prod003', 'Robô Aspirador de Pó KaBuM!');
 
 SELECT * FROM purchases;
 
 -- EDITAR PEDIDO
 UPDATE purchases
-SET total_price = 700 
-WHERE buyer_id = 'u003';
+SET total_price = 700 WHERE buyer_id = 'u003'
 
 -- JUNÇÃO DAS TABELAS
 
@@ -121,7 +121,7 @@ CREATE TABLE purchases_products (
 INSERT INTO purchases_products VALUES
 ('p001', 'prod003', 1),
 ('p001', 'prod005', 1 ),
-('p002', 'prod007', 2);
+('p002', 'prod006', 2);
 
 SELECT *
 FROM purchases_products
